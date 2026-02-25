3ed93c2d4887af575786a1cd480c061339dd8aef
dumbpenquin <yunuskumars@gmail.com>
2026-02-26 01:46:26 +0530

    Add maxDepth function for parentheses depth calculation
    Implement a solution to calculate the maximum nesting depth of parentheses.

---
+++ Day4/maximum-nesting-depth-of-the-parentheses.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxDepth(self, s):
+        count = 0
+        lists=[]
+        for x in s:
+            if x=='(':
+                count+=1
+            elif x==')':
+                lists.append(count)
+                count-=1
+        return max(lists)
